998c0b9e3ac8cf6f61a941bb119db4f4ac575fc8
DisRachik <radchenkoig@gmail.com>
2023-11-29 20:06:29 +0200

    task completed

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,52 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature = (): number => this.signature;
+}
+
+interface IPerson {
+  readonly key: Key;
+  getKey(): Key;
+}
+
+class Person implements IPerson {
+  constructor(public key: Key) {}
+
+  getKey = (): Key => this.key;
+}
+
+abstract class House {
+  protected isDoorOpen: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn = (tenants: Person): void => {
+    if (this.isDoorOpen) {
+      this.tenants.push(tenants);
+    } else {
+      console.log('If you are drunk or are you mistaken, but this is a stranger`s house!');
+    }
+  };
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.isDoorOpen = true;
+      this.comeIn(person);
+    }
+    this.isDoorOpen = false;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
-house.comeIn(person);
-
 
 export {};
